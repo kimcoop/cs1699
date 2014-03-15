249c1283992b0f0806013b11d57facb544e61eed
Kim Cooperrider <kim.cooperrider@gmail.com>
2014-03-15 13:54:25 -0400

    scaffold deliverable 3

---
+++ deliverable-3/features/step_definitions/example.steps.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = function() {
+
+  this.Given(/^I have an empty grocery list$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I add an item to the list$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^The grocery list contains a single item$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I can access that item from the grocery list$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+  
+};
