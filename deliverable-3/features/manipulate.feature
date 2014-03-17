b1477d3c0d0f5f71db4dac28350c6c2a4d8269d8
Kim Cooperrider <kim.cooperrider@gmail.com>
2014-03-16 21:40:36 -0400

    start of manipulate step defs

--- deliverable-3/features/manipulate.feature
+++ deliverable-3/features/manipulate.feature
@@ -5,24 +5,24 @@ Feature: Web developer can manipulate a date
 
   Scenario: Add 24 hours
     Given a valid reference to Moment.js
-    And today's date
+    Given today's date
     When I add 24 hours to the date
     Then I should see tomorrow's date
 
   Scenario: Subtract 24 hours
     Given a valid reference to Moment.js
-    And today's date
+    Given today's date
     When I subtract 24 hours from the date
     Then I should see yesterday's date
 
   Scenario: Add 7 days
     Given a valid reference to Moment.js
-    And today's date
+    Given today's date
     When I add 7 days to the date
     Then I should see the date 1 week ago
 
   Scenario: Subtract 7 days
     Given a valid reference to Moment.js
-    And today's date
+    Given today's date
     When I subtract 7 days from the date
     Then I should see the date 1 week ago
--- deliverable-3/features/step_definitions/manipulate.steps.js
+++ deliverable-3/features/step_definitions/manipulate.steps.js
@@ -2,12 +2,50 @@
 
 module.exports = function() {
   this.World = require("../support/world.js").World; // overwrite default World constructor
-
-  /*
-  this.Given(/^a thumbnail\-sized image on a webpage$/, function (callback) {
+  
+  this.Given(/^a valid reference to Moment\.js$/, function (callback) {
     // express the regexp above with the code you wish you had
     callback.pending();
   });
-  */
+
+  this.Given(/^today's date$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I add (\d+) hours to the date$/, function (arg1, callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should see tomorrow's date$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I subtract (\d+) hours from the date$/, function (arg1, callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should see yesterday's date$/, function (callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I add (\d+) days to the date$/, function (arg1, callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.Then(/^I should see the date (\d+) week ago$/, function (arg1, callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
+
+  this.When(/^I subtract (\d+) days from the date$/, function (arg1, callback) {
+    // express the regexp above with the code you wish you had
+    callback.pending();
+  });
 
 };
