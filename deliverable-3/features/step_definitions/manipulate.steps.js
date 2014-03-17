daaab00edfbcdbf3a81b160fb65021c26a51d4a7
Kim Cooperrider <kim.cooperrider@gmail.com>
2014-03-16 22:03:27 -0400

    adding assertions via chai to World and example in manipulate step defs

--- deliverable-3/features/step_definitions/manipulate.steps.js
+++ deliverable-3/features/step_definitions/manipulate.steps.js
@@ -2,49 +2,47 @@
 
 module.exports = function() {
   this.World = require("../support/world.js").World; // overwrite default World constructor
-  
+
   this.Given(/^a valid reference to Moment\.js$/, function (callback) {
-    // express the regexp above with the code you wish you had
-    callback.pending();
+    
+    var should = this.should,
+      correctYear = "1992",
+      mFormat = this.moment("Jun 18, 1992").format("YYYY");
+
+    mFormat.should.equal(correctYear);
+
+    callback();
   });
 
   this.Given(/^today's date$/, function (callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.When(/^I add (\d+) hours to the date$/, function (arg1, callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.Then(/^I should see tomorrow's date$/, function (callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.When(/^I subtract (\d+) hours from the date$/, function (arg1, callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.Then(/^I should see yesterday's date$/, function (callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.When(/^I add (\d+) days to the date$/, function (arg1, callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.Then(/^I should see the date (\d+) week ago$/, function (arg1, callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
   this.When(/^I subtract (\d+) days from the date$/, function (arg1, callback) {
-    // express the regexp above with the code you wish you had
     callback.pending();
   });
 
--- deliverable-3/features/support/world.js
+++ deliverable-3/features/support/world.js
@@ -1,8 +1,23 @@
 var moment = require('moment');
 
 var World = function World(callback) {
+
+  this.should = require('chai').should() //actually call the the function
+  this.expect = require('chai').expect;
   
-  console.log(moment("Jun 18, 1992").format("YYYY"));
+  this.moment = moment;
+
+  /*
+  we can use this.should within our step definitions:
+
+  foo.should.be.a('string');
+  foo.should.equal('bar');
+  foo.should.have.length(3);
+
+  - http://chaijs.com/guide/styles/
+  */
+  
+  // console.log(moment("Jun 18, 1992").format("YYYY"));
 
   callback(); // tell Cucumber we're finished and to use 'this' as the world instance
 };
