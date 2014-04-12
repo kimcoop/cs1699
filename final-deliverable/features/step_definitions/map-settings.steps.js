26ed431b1a5cf89885439cf08f7215d06ccdf6f0
Nicholas Amoscato <nicholas@amoscato.com>
2014-04-11 21:58:21 -0400

    Move WordPress login to world.js and simplify config

--- final-deliverable/features/step_definitions/map-settings.steps.js
+++ final-deliverable/features/step_definitions/map-settings.steps.js
@@ -1,49 +1,35 @@
 'use strict';
-
-var webdriverjs = require('webdriverjs'),
-    assert      = require('assert'),
-    config      = require('../support/config.js');
  
-var sharedSteps = module.exports = function(){
+var sharedSteps = module.exports = function() {
+  this.World = require('../support/world').World;
 
-    var client    = webdriverjs.remote({ desiredCapabilities: { browserName: 'phantomjs' }, logLevel: 'silent' }),
-        tmpResult = null,
-        config = this.config;
-
-    client.init();
- 
-    this.Given(/^I am on the page "([^"]*)"$/, function(url, next) {
-        client
-            .url(url)
-            .getTitle(function(title) {
-                if (title === "CS1699 Software Testing › Log In") {
-                    console.log('need to login');
-                    client.setValue("input[name=log]", config.username);
-                    client.setValue("input[name=pwd]", config.username);
-                    client.submitForm("form", function() {
-                        // send back to plugin settings page 
-                        client.url(url);
-                    });
-                }
-            })
-
-        client.call(next);
+  this.Given(/^I am on the page "([^"]*)"$/, function(url, next) {
+    
+    /*
+    this.client.setValue("#input-wtm-base-image", my.config.mapImage);
+    this.client.submitForm('.form-wtm', function() {
+        console.log( client.getTitle() );
+        assertEquals(config.mapImage, client.getAttribute(".link-wtm-image img", src));
     });
+    */
 
+    this.client.call(next);
 
-    this.When(/^I use enter the value "([^"]*)" on the input element "([^"]*)" and save$/, function (value, selector, next) {
+  });
 
-        client.setValue(selector, value);
-        client.submitForm('.form-wtm');
-        next();
+  this.When(/^I use enter the value "([^"]*)" on the input element "([^"]*)" and save$/, function (value, selector, next) {
 
-    });
+    this.client.setValue(selector, value);
+    this.client.submitForm('.form-wtm');
+    next();
 
-    this.Then(/^The source of the element "([^"]*)" should be "([^"]*)"$/, function (selector, value, next) {
-        
-        assertEquals(value, client.getAttribute(selector, src));
-        next();
+  });
 
-    });
+  this.Then(/^The source of the element "([^"]*)" should be "([^"]*)"$/, function (selector, value, next) {
+    
+    assertEquals(value, this.client.getAttribute(selector, src));
+    next();
+
+  });
 
 };
--- final-deliverable/features/support/world.js
+++ final-deliverable/features/support/world.js
@@ -1,4 +1,41 @@
+var webdriver = require('webdriverjs'),
+    assert    = require('assert'),
+    config    = require('./config');
+
 var World = function World(callback) {
+  var my = this;
+
+  my.webdriver = webdriver;
+  my.assert = assert;
+  my.config = config;
+
+  // initialize webdriverjs
+  my.client = my.webdriver.remote({desiredCapabilities: {browserName: 'phantomjs'}, logLevel: 'silent'});
+  my.client.init();
+
+  // login to wordpress
+  my.client
+    .url('http://labs.amoscato.com/cs1699-wordpress/wp-login.php')
+    .getTitle(function(err, title) {
+      assert(err === null, 'getTitle() returns with an error');
+
+      if (title === "CS1699 Software Testing › Log In") {
+        my.client.setValue("input[name=log]", my.config.username);
+        my.client.setValue("input[name=pwd]", my.config.username);
+        my.client.submitForm("form", function(err) {
+          assert(err === null, 'submitForm() returns with an error');
+          /*
+          console.log("submitted form");
+          
+          my.client.getTitle(function(err, title) {
+            assert(err === null, 'getTitle() returns with an error');
+            console.log(title);
+          });
+          */
+        });
+      }
+    });
+
   callback();
 };
 
